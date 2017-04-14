a4e713344d70b1010803fe17ba5c12712fbfc39d
havenwei <havenwei82@163.com>
2017-04-14 15:43:23 +0800

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
       end
       resources :resumes
     end
-
-
   end
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
