854c33066a0ff267a248d6f020627a85fe54dda0
havenwei <havenwei82@163.com>
2017-04-14 13:09:29 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,10 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      resources :resumes
     end
+
+
   end
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
