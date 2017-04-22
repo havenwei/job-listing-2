a6881243277e88c5c03700df9425d5d6dcd92f72
havenwei <havenwei82@163.com>
2017-04-22 20:17:03 +0800

    show jobs in table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,29 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require bootstrap/modal
 //= require_tree .
+
+$(document).ready(function() {
+
+	(function ($) {
+		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+
+		$('.tab ul.tabs li a').click(function (g) {
+			var tab = $(this).closest('.tab'),
+				index = $(this).closest('li').index();
+
+			tab.find('ul.tabs > li').removeClass('current');
+			$(this).closest('li').addClass('current');
+
+			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+
+			g.preventDefault();
+		} );
+	})(jQuery);
+
+});
